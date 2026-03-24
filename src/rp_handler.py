a44132f23388bf963d876c3eafd2f2a5c306965f
ttkengo <ttkengo@gmail.com>
2026-03-25 00:47:25 +0900

    Update rp_handler.py

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -42,7 +42,43 @@ def run_whisper_job(job):
             return {"error": input_validation['errors']}
         job_input = input_validation['validated_input']
 
-    # 入力ソースの判定
     if job_input.get('youtube_url', False):
         audio_input = youtube_to_tempfile(job_input['youtube_url'])
-    elif job_input.get('audio', False) and not job_input.get('audio_
+    elif job_input.get('audio', False) and not job_input.get('audio_base64', False):
+        with rp_debugger.LineTimer('download_step'):
+            audio_input = download_files_from_urls(job['id'], [job_input['audio']])[0]
+    elif job_input.get('audio_base64', False):
+        audio_input = base64_to_tempfile(job_input['audio_base64'])
+    else:
+        return {'error': 'Must provide youtube_url, audio, or audio_base64'}
+
+    with rp_debugger.LineTimer('prediction_step'):
+        whisper_results = MODEL.predict(
+            audio=audio_input,
+            model_name=job_input["model"],
+            transcription=job_input["transcription"],
+            translation=job_input["translation"],
+            translate=job_input["translate"],
+            language=job_input["language"],
+            temperature=job_input["temperature"],
+            best_of=job_input["best_of"],
+            beam_size=job_input["beam_size"],
+            patience=job_input["patience"],
+            length_penalty=job_input["length_penalty"],
+            suppress_tokens=job_input.get("suppress_tokens", "-1"),
+            initial_prompt=job_input["initial_prompt"],
+            condition_on_previous_text=job_input["condition_on_previous_text"],
+            temperature_increment_on_fallback=job_input["temperature_increment_on_fallback"],
+            compression_ratio_threshold=job_input["compression_ratio_threshold"],
+            logprob_threshold=job_input["logprob_threshold"],
+            no_speech_threshold=job_input["no_speech_threshold"],
+            enable_vad=job_input["enable_vad"],
+            word_timestamps=job_input["word_timestamps"]
+        )
+
+    with rp_debugger.LineTimer('cleanup_step'):
+        rp_cleanup.clean(['input_objects'])
+
+    return whisper_results
+
+runpod.serverless.start({"handler": run_whisper_job})
